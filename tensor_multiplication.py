015b092ad0711e27d814beb3f20a068690faece0
mabuhannood <122485641+mabuhannood@users.noreply.github.com>
2024-05-20 17:39:55 -0400

    code cleaning

--- tensor_multiplication.py
+++ tensor_multiplication.py
@@ -1,12 +1,10 @@
 import torch
 
 def create_tensor_of_val(dimensions, val):
-
     res = torch.ones(dimensions) * val
     return res
 
 def calculate_elementwise_product(A, B):
-
     if A.shape != B.shape:
         raise RuntimeError("dimensions of A and B should be the same.")
     res = A * B 
@@ -14,23 +12,19 @@ def calculate_elementwise_product(A, B):
 
 
 def calculate_matrix_product(X, W):
-
     res = torch.matmul(X, W.T)
     return res
 
 def calculate_matrix_prod_with_bias(X, W, b):
-
     mat_mul = torch.matmul(X, W.T)
     res = mat_mul + b
     return res
 
 def calculate_activation(sum_total):
-
     res = torch.heaviside(sum_total, torch.tensor(0.0))
     return res
 
 def calculate_output(X, W, b):
- 
     sum_total = calculate_matrix_prod_with_bias(X, W, b)
     res = calculate_activation(sum_total)
     return res
